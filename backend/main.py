58adcb5ac57db211153ba7228bf2e6cb467f6dac
Eduardo Fonseca da Silva <edufon1503@gmail.com>
2025-02-03 16:19:13 -0300

    Explicação de cada biblioteca utilizada

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, File, UploadFile
+
+# Middleware do FastAPI para controlar as permissões de CORS (Cross-Origin Resource Sharing). Isso permite configurar DE QUAIS domínios ou portas o servidort pode receber requisições
 from fastapi.middleware.cors import CORSMiddleware
+
+# Utilizado para manipulação dos diretórios
 import os 
+
+# Módulo para manipulação de arquivos. Aqui está sendo utilizado para copiar o arquivo recebido, salvando-o
 import shutil
+
+# Para processar o html
 from bs4 import BeautifulSoup
 
 
@@ -9,6 +17,8 @@ app = FastAPI()
 
 UPLOAD_DIR = "uploads"
 
+
+# Define as origens de onde pode receber requisições
 origins = [
     "http://localhost:5173"
 ]
@@ -17,12 +27,14 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_methods=['*'],
-    allow_headers=['*']
+    allow_methods=['*'], # Permite todos os métodos HTTP (desnecessario aqui).
+    allow_headers=['*'] # Permite todos os cabeçalhos nas requisições (desnecessario aqui)
 )
 
 
 # a palavra-chave async nas funções de endpoint permite que a função seja executada de maneira assíncrona, sem bloquear outras requisições.
+# Essa é uma das vantagens da FastAPI, que já suporta programação assíncrona nativamente
+
 @app.post(path="/upload/")
 async def upload_html(file: UploadFile = File(...)):
     """Recebe um arquivo HTML e salva no servidor"""
@@ -43,11 +55,12 @@ async def upload_html(file: UploadFile = File(...)):
         content = html_file.read()
         soup = BeautifulSoup(content, "html.parser")
         
-        # Procura pela tag <meta> que define o charset
+        # Procura pelas tags <meta>, que define o charset
         meta_tags = soup.find_all("meta") # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find
         
         found_charset = False
 
+        # Vai tentando encontrar o charsert
         for meta_tag in meta_tags:
             if "charset" in meta_tag.attrs["content"]:
                 charset = meta_tag.attrs["content"]
